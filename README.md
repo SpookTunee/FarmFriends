657da806344ddd15dbc8cc66e2772572c2613122
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-02-17 21:30:14 -0500

    merge from manual-merge-all

---
+++ README.md
@@ -0,0 +1,11 @@
+# Farm Friends
+
+This is a project created by Tate, Sarp, Rohan, and David for the *Advanced Video Game Design* class.
+
+TODO:
+
+ - Clean up code
+   - Update multiplayer code to be more consistent
+   - Comment code
+ - Create procedural map based on player count
+ - 
---
+++ source/Player/Scripts/Player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 10.0
+var camera_sense = 0.005
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 10.0
+
+func _enter_tree():
+	print(str(name).to_int())
+	set_multiplayer_authority(str(name).to_int())
+
+func _ready():
+
+	if not is_multiplayer_authority():
+		return
+	$Camera3D.make_current()
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	
+	
+func _unhandled_input(event):
+	if not is_multiplayer_authority(): 
+		return
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * camera_sense)
+		$Camera3D.rotate_x(-event.relative.y * camera_sense)
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -PI/2, PI/2)
+		
+
+func _physics_process(delta):
+
+	if !multiplayer.multiplayer_peer: return
+	
+	if !is_multiplayer_authority(): return
+	
+	
+	$Camera3D.make_current()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+
+
+	move_and_slide()
