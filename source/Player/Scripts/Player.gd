667d2692060d497b5dbe9bc783a7f46edb60c469
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-02-29 11:47:45 -0500

    change dn

--- source/Player/Scripts/Player.gd
+++ source/Player/Scripts/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 20.0
+const JUMP_VELOCITY = 5.0
 var camera_sense = 0.005
 @onready var Hand = $Camera3D/Hand
 var hoe = preload("res://Prototype/Hoe.tscn")
