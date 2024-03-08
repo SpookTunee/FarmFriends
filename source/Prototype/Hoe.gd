3879fb441119cd6a1f77c3f2c0f0d8ac95546862
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-03-08 09:59:10 -0500

    Replace main with planting-stuff

--- source/Prototype/Hoe.gd
+++ source/Prototype/Hoe.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-var tilLand = preload("res://Prototype/Plant_prototype/tilled_land.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,18 +11,8 @@ func _process(delta):
 
 func activate():
 	$AnimationPlayer.play("hoe")
-	#var npos = Vector2i(0,0)
-	#npos.x = abs(int(
-		#(global_position.x-50.0)*(64.0/100.0)
-	#))
-	#npos.y = abs(int(
-		#(global_position.z-50.0)*(64.0/100.0)
-	#))
-	#npos = Vector2i(64,64)-npos
-	#var tpos = get_node("/root/World/Terrain").global_position + Vector3(float(npos.x)*(100.0/64.0),float(sin((float(npos.x)*(100.0/64.0))/8)*cos((float(npos.y)*(100.0/64.0))/8)*2), float(npos.y)*(100.0/64.0)) - Vector3(50, 0, 50)
-	var Look = get_parent().get_parent().get_child(0)
-	if Look.get_collider() && (Look.get_collider().name == "Terrain"):
-		var till = tilLand.instantiate()
-		get_node("/root/World").add_child(till)
-		#var tpos = Vector3(int(Look.get_collision_point().x*1000)%int((64.0/100.0)*1000),int(Look.get_collision_point().y*1000)%int((64.0/100.0)*1000),int(Look.get_collision_point().z*1000)%int((64.0/100.0)*1000))/1000
-		till.position = Look.get_collision_point()
+	if multiplayer.multiplayer_peer:
+		var Look = get_parent().get_parent().get_child(0)
+		if Look.get_collider() && (Look.get_collider().name == "Terrain"):
+			$/root/World/TilledLand.instant_till(multiplayer.get_unique_id(),$/root/World/TilledLand.till_count+1,Look.get_collision_point())
+			$/root/World/TilledLand.spawn_till.rpc($/root/World/TilledLand.till_count,Look.get_collision_point())
