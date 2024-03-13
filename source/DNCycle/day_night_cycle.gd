38705291243bec9f41d0f33e13648cb854b27b86
Rohan Belanger <bebisbebiskhan@gmail.com>
2024-03-13 11:44:19 -0400

    Global vars / added day system

--- source/DNCycle/day_night_cycle.gd
+++ source/DNCycle/day_night_cycle.gd
@@ -2,15 +2,22 @@ extends Node3D
 
 @export var cycle_sec: int = 30
 var starting_rotation = -35
-
+@onready var daycheck : bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$DirectionalLight3D.rotation.x = starting_rotation*PI/180
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	$DirectionalLight3D.rotation.x += 360*delta/cycle_sec*PI/180
 	$DirectionalLight3D.light_energy = 1+(cos($DirectionalLight3D.rotation.x)/2)
 	if $DirectionalLight3D.rotation.x >= 2*PI:
 		$DirectionalLight3D.rotation.x = 0
+	if $DirectionalLight3D.rotation_degrees.x > 90 - 5 && $DirectionalLight3D.rotation_degrees.x < 90 + 5 && daycheck:
+		daycheck = false
+		Global.day += 1
+	if $DirectionalLight3D.rotation_degrees.x >= -10 && $DirectionalLight3D.rotation_degrees.x <= 10:
+		daycheck = true
---
+++ source/globals.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var day : int
+var dayName : String
+
+
+func _process(delta):
+	dayCalc()
+
+
+func dayCalc():
+	var weekday = day % 7
+	
+	if weekday == 0:
+		dayName = "Sunday"
+	if weekday == 1:
+		dayName = "Monday"
+	if weekday == 2:
+		dayName = "Tuesday"
+	if weekday == 3:
+		dayName = "Wednesday"
+	if weekday == 4:
+		dayName = "Thursday"
+	if weekday == 5:
+		dayName = "Friday"
+	if weekday == 6:
+		dayName = "Saturday"
