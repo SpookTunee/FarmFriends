64efe8c463c13e9e7d31368dda7310755d1deed8
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-03-14 11:20:23 -0400

    sythe workey. woekey

--- source/Prototype/Scythe.gd
+++ source/Prototype/Scythe.gd
@@ -1,15 +1,35 @@
 extends Node3D
 
+var cutting = false
+var cooldown = false
+
+func _ready():
+	$scythe/es.timeout.connect(_es_end)
+	$scythe/ss.timeout.connect(_ss_end)
+	
+func _physics_process(_delta):
+	if cutting:
+		if multiplayer.multiplayer_peer:
+			var Look = $scythe/Area3D
+			var con
+			for i in Look.get_overlapping_areas():
+				if i.name == "PlantBox":
+					con = i
+					break
+			if con:
+				#if !con.get_parent().get_node("Plant/PlantBody/AnimationPlayer").is_playing():
+				con.get_parent().get_parent().harvest_plant()
 
 func activate():
 	$scythe/AnimationPlayer.play("scythe")
-	if multiplayer.multiplayer_peer:
-		var Look = $scythe/Area3D
-		var con
-		for i in Look.get_overlapping_areas():
-			if i.name == "PlantBox":
-				con = i
-				break
-		if con:
-			#if !con.get_parent().get_node("Plant/PlantBody/AnimationPlayer").is_playing():
-			con.get_parent().get_parent().harvest_plant()
+	if !cooldown:
+		$scythe/ss.start()
+		cooldown = true
+		cutting = true
+
+func _ss_end():
+	cutting = false
+	$scythe/es.start()
+	
+func _es_end():
+	cooldown = false
