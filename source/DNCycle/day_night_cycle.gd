27f07f378b856ba51c25aea1a5bcc12b28cdd09e
Rohan Belanger <bebisbebiskhan@gmail.com>
2024-03-19 11:34:11 -0400

    day night cycle fixes

--- source/DNCycle/day_night_cycle.gd
+++ source/DNCycle/day_night_cycle.gd
@@ -8,12 +8,14 @@ func _ready():
 	$DirectionalLight3D.rotation.x = starting_rotation*PI/180
 	
 
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	$DirectionalLight3D.rotation.x += 360*delta/cycle_sec*PI/180
+	$DirectionalLight3D2.rotation.x = -$DirectionalLight3D.rotation.x
 	$DirectionalLight3D.light_energy = 1+(cos($DirectionalLight3D.rotation.x)/2)
+	$DirectionalLight3D2.light_energy = 1+(cos(-($DirectionalLight3D.rotation.x))/2) * 1.5 + 0.5
 	if $DirectionalLight3D.rotation.x >= 2*PI:
 		$DirectionalLight3D.rotation.x = 0
 	if $DirectionalLight3D.rotation_degrees.x > 90 - 5 && $DirectionalLight3D.rotation_degrees.x < 90 + 5 && daycheck:
--- source/Prototype/truck_prototype.gd
+++ source/Prototype/truck_prototype.gd
@@ -1,11 +1,18 @@
 extends Node3D
 
-
+var check : bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("TruckEnter")
+	position.y = 100000000
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Global.dayName == "Saturday":
+		position.y = 0
+		if check:
+			$AnimationPlayer.play("TruckEnter")
+			check = false
+	if Global.dayName != "Saturday" || Global.dayName != "Sunday":
+		check = true
+		position.y = 100000000
