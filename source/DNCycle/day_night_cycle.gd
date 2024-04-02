e737e5d72be3ed181b4d16d680d445f816a6a307
Rohan Belanger <bebisbebiskhan@gmail.com>
2024-04-02 11:05:16 -0400

    Day and night lighting fixes!

--- source/DNCycle/day_night_cycle.gd
+++ source/DNCycle/day_night_cycle.gd
@@ -14,8 +14,10 @@ func _process(delta):
 	Global.dayfloat += (delta/cycle_sec)
 	Global.day = floor(Global.dayfloat)
 	$DirectionalLight3D.rotation.x += deg_to_rad(360.0*(delta/cycle_sec))
-	$DirectionalLight3D2.rotation.x = -$DirectionalLight3D.rotation.x
+	$DirectionalLight3D2.rotation.x = -$DirectionalLight3D.rotation.x +45
+	$DirectionalLight3D3.rotation.x = -$DirectionalLight3D.rotation.x -45
 	$DirectionalLight3D.light_energy = 1+(cos($DirectionalLight3D.rotation.x)/2)
-	$DirectionalLight3D2.light_energy = 1+(cos(-($DirectionalLight3D.rotation.x))/2) * 1.5 + 0.5
+	$DirectionalLight3D2.light_energy = 1+(cos(-($DirectionalLight3D.rotation.x))/2) * 1.5 + 0.8
+	$DirectionalLight3D3.light_energy = 1+(cos(-($DirectionalLight3D.rotation.x))/2) * 1.5 + 0.8
 	if $DirectionalLight3D.rotation.x >= 2*PI:
 		$DirectionalLight3D.rotation.x = 0
