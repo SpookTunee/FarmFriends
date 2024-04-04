35d7fd8434360b5772e4a49a7bfc8cd0afb387ee
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-04-04 11:47:32 -0400

    random shit

--- source/Tools/Scripts/Hoe.gd
+++ source/Tools/Scripts/Hoe.gd
@@ -15,8 +15,8 @@ func activate():
 		var Look = get_parent().get_parent().get_child(0)
 		if Look.get_collider() && (Look.get_collider().name == "Terrain"):
 			var lpos = Look.get_collision_point()
-			lpos.x = (int(lpos.x)/1.25)*1.25 - 0.625
-			lpos.z = (int(lpos.z)/1.25)*1.25 - 0.625
+			lpos.x = float(floor(lpos.x)) + 0.5
+			lpos.z = float(floor(lpos.z)) + 0.5
 			if !(Vector2((lpos.x),(lpos.z)) in get_node("/root/World/TilledLand").tillposs):
 				get_node("/root/World/TilledLand").tillposs.insert(0,Vector2(lpos.x,lpos.z))
 				$/root/World/TilledLand.instant_till(multiplayer.get_unique_id(),$/root/World/TilledLand.till_count+1,lpos)
