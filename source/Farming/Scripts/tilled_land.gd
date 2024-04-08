9f4c762d9297fd9a4a9d4a74d29423fd91610d9b
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-04-08 18:03:26 -0400

    Water mechinics haldf done L:)

--- source/Farming/Scripts/tilled_land.gd
+++ source/Farming/Scripts/tilled_land.gd
@@ -1,16 +1,24 @@
 extends Node3D
 
 var wheatplant = preload("res://Farming/plant_prototype.tscn")
+@export var water_level:float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
+func reduce_water():
+	if water_level > (1.0/12.0):
+		water_level = water_level - (1.0/12.0)
+		$CSGTorus3D.material.albedo_color = Color.from_hsv(31.0/255.0,1.0,(43.0-(water_level*20))/100.0,1.0)
 
 func grow(plant):
 	if get_node_or_null("PLANT"): return
 	gplant(plant)
 	$/root/World/TilledLand.grow_plant.rpc(self.name,plant)
+
+#H  S   V  A
+#31 100 34 255
 
 func gplant(plant):
 	var growplant = get_node("/root/World/PlantSpawner").quick_init(plant)
