ac57b52905b17db8759acd1919ed1386b9235ee5
Rohan Belanger <bebisbebiskhan@gmail.com>
2024-05-03 10:17:00 -0400

    wtf

--- source/DNCycle/day_night_cycle.gd
+++ source/DNCycle/day_night_cycle.gd
@@ -2,8 +2,12 @@ extends Node3D
 
 @export var cycle_sec: int = 30
 var starting_rotation = 210
+var check : bool = true
+var currday : int
+signal dayChange
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	currday = floor(Global.dayfloat)
 	$DirectionalLight3D.rotation.x = starting_rotation*PI/180
 	
 func is_day():
@@ -13,6 +17,9 @@ func is_day():
 func _process(delta):
 	Global.dayfloat += (delta/cycle_sec)
 	Global.day = floor(Global.dayfloat)
+	if currday != Global.day:
+		dayChange.emit()
+		currday = Global.day
 	$DirectionalLight3D.rotation.x += deg_to_rad(360.0*(delta/cycle_sec))
 	$DirectionalLight3D2.rotation.x = -$DirectionalLight3D.rotation.x +45
 	$DirectionalLight3D3.rotation.x = -$DirectionalLight3D.rotation.x -45
@@ -22,8 +29,12 @@ func _process(delta):
 	if $DirectionalLight3D.rotation.x >= 2*PI:
 		$DirectionalLight3D.rotation.x = 0
 		
-		
+	
 		
 	
-	if Global.get_day_name() == "Monday" && (Global.dayfloat - float(Global.day)) < delta && Global.day > 3:
-		Global.quotaPrice * 1.1
+	if Global.get_day_name() == "Monday" && check && Global.day > 3:
+		check = false
+		Global.quotaPrice *= 1.5
+		
+	if Global.get_day_name() != "Monday":
+		check = true
