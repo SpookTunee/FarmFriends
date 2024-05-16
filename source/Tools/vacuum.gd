e2d41ac55826701ef0f451bb375365dfa6da9062
Rohan Belanger <bebisbebiskhan@gmail.com>
2024-05-15 23:02:19 -0400

    reworked vacuum

--- source/Tools/vacuum.gd
+++ source/Tools/vacuum.gd
@@ -3,23 +3,25 @@ extends Node3D
 var inRad : Array
 var force_dir : Vector3
 var active : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+	
+func activate():
+	pass
 
 func _physics_process(delta):
-	if Input.is_action_pressed("m1"):
-		for i in $Area3D.get_overlapping_areas():
-			if i.name == "Hitbox":
-				if i.get_parent() != self.get_parent().get_parent().get_parent():
+	for i in $Area3D.get_overlapping_areas():
+		if i.name == "Hitbox":
+			if i.get_parent() != self.get_parent().get_parent().get_parent():
 					
-					force_dir = Vector3(0,0,0) - Vector3($Origin.global_position.x - (i.get_parent().global_position.x), -($Origin.global_position.y - (i.get_parent().global_position.y)), $Origin.global_position.z - (i.get_parent().global_position.z)) # finds direction
-					#print(force_dir)
-					#force_dir = Vector3(0,0,0) - force_dir
+				force_dir = (Vector3(0,0,0) - Vector3($Origin.global_position.x - (i.get_parent().global_position.x), -($Origin.global_position.y - (i.get_parent().global_position.y)), $Origin.global_position.z - (i.get_parent().global_position.z))) # finds direction
+				#print(force_dir)
+				#force_dir = Vector3(0,0,0) - force_dir
 
-					i.get_parent().pushPull(force_dir, delta)
-			
+				i.get_parent().pushPull(force_dir, delta)
+	
 
 
 
@@ -35,8 +37,7 @@ func _physics_process(delta):
 
 
 # THIS NEEDS TO ACTIVATE ALWAYS NOT JUST ONCE -- CHANGE PLAYER SCRIPT
-func activate():
-	pass
+
 
 
 func _on_area_3d_area_exited(area):
