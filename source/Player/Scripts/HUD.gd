f11978ba05c497723a6dda1208777cc294464065
SpookTunee <tate.scarbrough01@gmail.com>
2024-05-21 11:39:35 -0400

    hababadhuh

--- source/Player/Scripts/HUD.gd
+++ source/Player/Scripts/HUD.gd
@@ -4,13 +4,13 @@ extends Control
 
 func _process(delta):
 	$Day.text = Global.get_day_name()
-	$Inventory/Wheat.text = str(get_node("../Stats").crop_counts[Global.Plants.WHEAT]) + " Wheat"
-	$Inventory/Corn.text = str(get_node("../Stats").crop_counts[Global.Plants.CORN]) + " Corn"
-	$Inventory/Potatoes.text = str(get_node("../Stats").crop_counts[Global.Plants.POTATO]) + " Potatoes"
-	$Inventory/Carrots.text = str(get_node("../Stats").crop_counts[Global.Plants.CARROT]) + " Carrots"
-	$Inventory/Mushrooms.text = str(get_node("../Stats").crop_counts[Global.Plants.MUSHROOM]) + " Mushrooms"
-	$Inventory/Money.text = "$" + str(get_node("../Stats").money)
+	$Inventory/Wheat.text = str(get_node("../Stats").crop_counts[Global.Plants.WHEAT]) +  "   "
+	$Inventory/Corn.text = str(get_node("../Stats").crop_counts[Global.Plants.CORN]) + "   "
+	$Inventory/Potatoes.text = str(get_node("../Stats").crop_counts[Global.Plants.POTATO]) + "   "
+	$Inventory/Carrots.text = str(get_node("../Stats").crop_counts[Global.Plants.CARROT]) + "   "
+	$Inventory/Mushrooms.text = str(get_node("../Stats").crop_counts[Global.Plants.MUSHROOM]) + "   "
+	$Inventory/Money.text = str(get_node("../Stats").money) + "   "
 	$Quota.text = "DUE: " + str(Global.quotaPrice + get_parent().addedQuota - get_node("../Stats").moneyPaid)
-	
+	#if get_parent().
 	
 	
