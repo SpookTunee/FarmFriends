e22ccd210cbab691c1f83cf0a0acec48ca039deb
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2024-05-30 11:05:51 -0400

    adfgadfasefaa awfauhsdkfygbauydsbfj

--- source/Tools/mine_placer.gd
+++ source/Tools/mine_placer.gd
@@ -2,5 +2,7 @@ extends Node3D
 
 func activate():
 	if get_node("../../../MineCooldown").is_stopped():
-		get_node("/root/World/Mines").minePlace.rpc($Spawner.global_position)
-		get_node("../../../MineCooldown").start()
+		if get_node("../../..").item_state["misc"]["mine"]["count"] > 0:
+			get_node("/root/World/Mines").minePlace.rpc($Spawner.global_position)
+			get_node("../../../MineCooldown").start()
+			get_node("../../..").item_state["misc"]["mine"]["count"] -= 1
