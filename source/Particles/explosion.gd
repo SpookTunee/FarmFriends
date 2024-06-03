a356717b6fe284e69fba3beca9ac2636d19fda06
Rohan Belanger <bebisbebiskhan@gmail.com>
2024-06-03 11:24:44 -0400

    Explosion Sound

--- source/Particles/explosion.gd
+++ source/Particles/explosion.gd
@@ -1,11 +1,12 @@
 extends Node3D
 
 var size = 1
+var pitch : float = 1.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	var randomize : float = randf_range(-0.10, 0.50)
+	$ExplosionSound.set_pitch_scale(1 + randomize)
+	$ExplosionSound.play()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -20,3 +21,8 @@ func rppeed():
 	for i in range(0, $Node3D.get_child_count()):
 		$Node3D.get_child(i).scale *= size
 		$Node3D.get_child(i).set_emitting(true)
+
+
+@rpc("any_peer", "call_local", "unreliable")
+func playSound():
+	$ExplosionSound.play()
